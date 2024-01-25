f22960b8f6cdfaee4e824c85d38eca5a9f04213d
zwk <zhangweikun@yijianar.com>
2024-01-25 18:27:25 +0800

    support global function

--- src/main/groovy/com/zwk/RuleContext.groovy
+++ src/main/groovy/com/zwk/RuleContext.groovy
@@ -21,38 +21,25 @@ class RuleContext {
     }
 
     def registerFunction(String funcName, Object target, String methodName = funcName) {
-        Objects.requireNonNull(funcName, "funcName cannot be null")
-        Objects.requireNonNull(target, "target cannot be null")
-        if (functions.containsKey(funcName)) {
-            throw new IllegalArgumentException("function ${funcName} already exists!")
-        }
-        functions[funcName] = new RuleFunction(funcName: funcName, target: target, methodName: methodName)
+        registerFunction(new RuleFunction(funcName: funcName, target: target, methodName: methodName))
     }
 
     def registerFunction(RuleFunction ruleFunction) {
-        Objects.requireNonNull(ruleFunction, "rule function cannot be null")
-        Objects.requireNonNull(ruleFunction.funcName, "rule function funcName cannot be null")
-        Objects.requireNonNull(ruleFunction.target, "rule function target cannot be null")
-        if (functions.containsKey(ruleFunction.funcName)) {
-            throw new IllegalArgumentException("function ${ruleFunction.funcName} already exists!")
-        }
-        functions[ruleFunction.funcName] = ruleFunction
+        RuleFunction.registerFunction(functions, ruleFunction)
     }
 
-    def registerFunction(Map<String, RuleFunction> functions) {
-        def thiz = this
-        functions.each { k, v ->
-            if (thiz.functions.containsKey(k)) {
-                throw new IllegalArgumentException("function ${k} already exists!")
-            }
-            thiz.functions[k] = v
+    def registerFunction(List<RuleFunction> functions) {
+        functions.each {
+            registerFunction(it)
         }
     }
 
     def callFunction(String funcName, Object[] args) {
         def rf = functions[funcName]
-        if (rf == null) {
-            throw new IllegalArgumentException("function $funcName not exists!")
+        if (!rf) {
+            rf = RuleFunction.globalFunctions[funcName]
+            if (!rf)
+                throw new IllegalArgumentException("function $funcName not exists!")
         }
         def methodName = rf.methodName
         if (!methodName) {
--- src/main/groovy/com/zwk/RuleFunction.groovy
+++ src/main/groovy/com/zwk/RuleFunction.groovy
@@ -1,7 +1,34 @@
 package com.zwk
 
 class RuleFunction {
+    static final def globalFunctions = [:]
+
     def funcName
     def target
     def methodName
+
+
+    static def registerGlobalFunction(String funcName, Object target, String methodName = funcName) {
+        registerGlobalFunction(new RuleFunction(funcName: funcName, target: target, methodName: methodName))
+    }
+
+    static def registerGlobalFunction(RuleFunction ruleFunction) {
+        registerFunction(globalFunctions, ruleFunction)
+    }
+
+    static def registerGlobalFunction(List<RuleFunction> functions) {
+        functions.each {
+            registerGlobalFunction(it)
+        }
+    }
+
+    static def registerFunction(Map functions, RuleFunction ruleFunction) {
+        Objects.requireNonNull(ruleFunction, "rule function cannot be null")
+        Objects.requireNonNull(ruleFunction.funcName, "rule function funcName cannot be null")
+        Objects.requireNonNull(ruleFunction.target, "rule function target cannot be null")
+        if (functions.containsKey(ruleFunction.funcName)) {
+            throw new IllegalArgumentException("function ${ruleFunction.funcName} already exists!")
+        }
+        functions[ruleFunction.funcName] = ruleFunction
+    }
 }
